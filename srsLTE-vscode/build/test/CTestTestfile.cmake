458442b7bd6186d83cc02c6fa08af1b84729bf3f
Baojiazhong <1422401429@qq.com>
2021-03-16 15:05:13 +0800

    Add ce data collector

---
+++ srsLTE-vscode/build/test/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/hadoop/Bjz_workplace/srsLTE-github/srsLTE-vscode/test
+# Build directory: /home/hadoop/Bjz_workplace/srsLTE-github/srsLTE-vscode/build/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
